30d21485c3f35cb31f87ec75d7b0a9df5755177b
HeCG95 <jacktanger@163.com>
2023-03-25 11:27:42 +0800

    [M] @Indexed 2023-3-25 11:27:36

--- springboot/auto-config-new/src/main/java/pkg/WebConfig.java
+++ springboot/auto-config-new/src/main/java/pkg/WebConfig.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.BeanFactory;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Indexed;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,6 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class WebConfig {
 
     @RestController
+    @Indexed
     static class DemoCtrl {
         @GetMapping("hello")
         @Bean
